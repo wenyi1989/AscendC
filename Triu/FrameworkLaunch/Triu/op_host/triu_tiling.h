59da65bf7182e738f6a43f7ec74e158c5032a9d2
wenyi1989 <820589834@qq.com>
2024-09-27 10:30:57 +0800

    Add files via upload

---
+++ Triu/FrameworkLaunch/Triu/op_host/triu_tiling.h
@@ -0,0 +1,21 @@
+
+#include "register/tilingdata_base.h"
+
+namespace optiling {
+BEGIN_TILING_DATA_DEF(TriuTilingData)
+  TILING_DATA_FIELD_DEF(uint32_t, totalLength);
+  TILING_DATA_FIELD_DEF(uint32_t, tileNum);
+  TILING_DATA_FIELD_DEF(uint32_t, ALIGN_NUM);
+  TILING_DATA_FIELD_DEF(uint32_t, tiling_size);
+  TILING_DATA_FIELD_DEF(uint32_t, block_size);
+  TILING_DATA_FIELD_DEF(uint32_t, aivNum);
+  TILING_DATA_FIELD_DEF(uint32_t, core_size);
+  TILING_DATA_FIELD_DEF(uint32_t, core_remain);
+  TILING_DATA_FIELD_DEF(int32_t, diagonal);
+  TILING_DATA_FIELD_DEF(int32_t, dimnuma);
+  TILING_DATA_FIELD_DEF(int32_t, dimnumb);
+  TILING_DATA_FIELD_DEF(int32_t, dimnumbalign);
+END_TILING_DATA_DEF;
+
+REGISTER_TILING_DATA_CLASS(Triu, TriuTilingData)
+}
